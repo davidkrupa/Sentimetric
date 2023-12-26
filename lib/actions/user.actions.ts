51f9793709cf517d4dfc24faf3828600f678ed64
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-12-26 18:40:58 +0100

    consols for debugging

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,10 +5,15 @@ import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 
 export const createUser = async (user: CreateUserParams) => {
+  console.log("ACTION: start");
   try {
     await connectToDatabase();
 
+    console.log("ACTION: after connection");
+
     const newUser = await User.create(user);
+
+    console.log(`ACTION: user created - ${JSON.stringify(newUser)}`);
 
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -8,10 +8,13 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  console.log("CONNECT: start");
   // If a connection already exists, return it immediately
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
+  console.log("CONNECT: no connction no error");
 
   // If there is no existing promise, create one connecting to the database
   cached.promise =
@@ -22,6 +25,8 @@ export const connectToDatabase = async () => {
     });
 
   cached.conn = await cached.promise;
+
+  console.log(`CONNECT: promise - ${JSON.stringify(cached.conn)}`);
 
   // Return the established connection
   return cached.conn;
