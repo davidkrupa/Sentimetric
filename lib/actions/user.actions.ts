e1a18059792470f29c8b5a9b60a85807188327c1
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-12-26 21:32:13 +0100

    debuging

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,23 +1,50 @@
+// "use server";
+
+// import { CreateUserParams } from "@/types";
+// import { connectToDatabase } from "../database";
+// import User from "../database/models/user.model";
+
+// export const createUser = async (user: CreateUserParams) => {
+//   try {
+//     await connectToDatabase();
+
+//     const newUser = await User.create(user);
+
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+//   }
+// };
+
 "use server";
 
-import { CreateUserParams } from "@/types";
-import { connectToDatabase } from "../database";
-import User from "../database/models/user.model";
+import { connectToDatabase } from "@/lib/database";
+import User from "@/lib/database/models/user.model";
+import { handleError } from "@/lib/utils";
 
-export const createUser = async (user: CreateUserParams) => {
-  console.log("ACTION: start");
+import { CreateUserParams } from "@/types";
+
+export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
 
-    console.log("ACTION: after connection");
-
     const newUser = await User.create(user);
-
-    console.log(`ACTION: user created - ${JSON.stringify(newUser)}`);
-
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
-    console.log(error);
-    throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+    handleError(error);
   }
-};
+}
+
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,22 +1,48 @@
-// Pattern for reduce number of connections in serverles environment - Vercel
+// // Pattern for reduce number of connections in serverles environment - Vercel
+
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// // Create an object to store the connection and promise
+// let cached = (global as any).mongoose || { conn: null, promise: null };
+
+// export const connectToDatabase = async () => {
+//   console.log("CONNECT: start");
+//   // If a connection already exists, return it immediately
+//   if (cached.conn) return cached.conn;
+
+//   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
+//   console.log("CONNECT: no connction no error");
+
+//   // If there is no existing promise, create one connecting to the database
+//   cached.promise =
+//     cached.promise ||
+//     mongoose.connect(MONGODB_URI, {
+//       dbName: "sentimetric",
+//       bufferCommands: false,
+//     });
+
+//   cached.conn = await cached.promise;
+
+//   console.log(`CONNECT: promise - ${JSON.stringify(cached.conn)}`);
+
+//   // Return the established connection
+//   return cached.conn;
+// };
 
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Create an object to store the connection and promise
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  console.log("CONNECT: start");
-  // If a connection already exists, return it immediately
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  console.log("CONNECT: no connction no error");
-
-  // If there is no existing promise, create one connecting to the database
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
@@ -26,8 +52,5 @@ export const connectToDatabase = async () => {
 
   cached.conn = await cached.promise;
 
-  console.log(`CONNECT: promise - ${JSON.stringify(cached.conn)}`);
-
-  // Return the established connection
   return cached.conn;
 };
