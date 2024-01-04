16a23373d53e632bfc27b132c39331c291bd5f82
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-01-04 22:10:39 +0100

    custom question to openai

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -2,7 +2,17 @@ import { getAiResponse } from "@/lib/openai";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const response = await getAiResponse("Hi how are you?");
+  async function streamToString(stream: any) {
+    const chunks = [];
+    for await (const chunk of stream) {
+      chunks.push(chunk);
+    }
+    return Buffer.concat(chunks).toString("utf8");
+  }
+
+  const data = await streamToString(req.body);
+
+  const response = await getAiResponse(data);
 
   return NextResponse.json(response);
 }
