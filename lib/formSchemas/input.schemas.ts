fa02feb57e6bcf8548b5bfdd290ca6fdbe3262d6
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-01-13 19:16:05 +0100

    form collecting job data changed to new format

--- lib/formSchemas/input.schemas.ts
+++ lib/formSchemas/input.schemas.ts
@@ -3,10 +3,14 @@ import * as z from "zod";
 export const FormSchema = z.object({
   jobTitle: z
     .string()
-    .min(3, {
-      message: "Job title must be at least 3 characters",
-    })
-    .max(40, {
-      message: "Job title must be at max 40 characters",
-    }),
+    .min(3, { message: "Job title must be at least 3 characters" })
+    .max(40, { message: "Job title must be at max 40 characters" }),
+  company: z
+    .string()
+    .min(3, { message: "Company name must be at least 3 characters" })
+    .max(60, { message: "Company name must be at max 60 characters" }),
+  industry: z
+    .string()
+    .min(3, { message: "Industry must be at least 3 characters" })
+    .max(40, { message: "Industry must be at max 40 characters" }),
 });
