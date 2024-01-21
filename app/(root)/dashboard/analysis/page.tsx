0155f6078c407aaf4e867a6dd67b983f6fb54512
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-01-21 20:55:20 +0100

    getting data from mongodb and passing it to table

--- app/(root)/dashboard/analysis/page.tsx
+++ app/(root)/dashboard/analysis/page.tsx
@@ -1,3 +1,4 @@
+import { DataTable } from "@/components/DataTable";
 import { ShadcnCustomAnalysisForm } from "@/components/ShadcnCustomAnalysisForm";
 import {
   Card,
@@ -8,32 +9,40 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { getAllAnalysis } from "@/lib/actions/analysis.actions";
+import { columns } from "@/lib/tables/columns";
 
-const page = () => {
+const page = async () => {
+  const data = await getAllAnalysis();
+
   return (
-    <div className="grid grid-cols-2 gap-6">
-      <Card>
-        <CardHeader>
-          <CardTitle>Custom Analysis</CardTitle>
-          <CardDescription>
-            Analyze areas that will allow to collect data on the company's goals
-            (job offer, about us page, news).
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ShadcnCustomAnalysisForm />
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Your Current Analysis</CardTitle>
-          <CardDescription>Topic from previous form</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ScrollArea className="h-72">Content of the analysis.</ScrollArea>
-        </CardContent>
-      </Card>
-    </div>
+    <main className="grid gap-6">
+      <div className="grid grid-cols-2 gap-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>Custom Analysis</CardTitle>
+            <CardDescription>
+              Analyze areas that will allow to collect data on the company's
+              goals (job offer, about us page, news).
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ShadcnCustomAnalysisForm />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Your Current Analysis</CardTitle>
+            <CardDescription>Topic from previous form</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ScrollArea className="h-72">Content of the analysis.</ScrollArea>
+          </CardContent>
+        </Card>
+      </div>
+
+      <DataTable columns={columns} data={data} />
+    </main>
   );
 };
 
