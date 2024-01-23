b6cb35e8e585b1fb4e3e76aae6899762172383f6
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-01-23 21:14:20 +0100

    profile model

---
+++ lib/database/models/profile.models.ts
@@ -0,0 +1,13 @@
+import { Schema, model, models } from "mongoose";
+
+const ProfileSchema = new Schema({
+  jobTitle: { type: String, required: true },
+  company: { type: String, required: true },
+  industry: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Profile = models.Profile || model("Profile", ProfileSchema);
+
+export default Profile;
