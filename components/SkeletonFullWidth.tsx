cf0edf5f6a9e29c5c3326905ea10af814e4589c4
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-02-11 23:11:16 +0100

    skeleton from shadcn ui

---
+++ components/SkeletonFullWidth.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "./ui/skeleton";
+
+const SkeletonFullWidth = () => {
+  return <Skeleton className="h-40 col-span-2 rounded-sm" />;
+};
+
+export default SkeletonFullWidth;
