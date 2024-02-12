6108412d90457374a6fcc35729e8545d23e5bd06
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-02-12 16:01:15 +0100

    moved data getting logic for table to separate component

---
+++ components/AnalysisTable.tsx
@@ -0,0 +1,17 @@
+import {
+  getAllAnalysis,
+  getCurrentAnalysis,
+} from "@/lib/actions/analysis.actions";
+import { DataTable } from "./DataTable";
+import { columns } from "@/lib/tables/columns";
+
+const AnalysisTable = async () => {
+  const data = await getAllAnalysis();
+  const currentAnalysis = await getCurrentAnalysis();
+
+  return (
+    <DataTable columns={columns} data={data} currentId={currentAnalysis?._id} />
+  );
+};
+
+export default AnalysisTable;
