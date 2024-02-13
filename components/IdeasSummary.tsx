3c140db40d401bb5f9c916e5f3c86499563b2a91
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-02-13 14:07:29 +0100

    ideas summary component

---
+++ components/IdeasSummary.tsx
@@ -0,0 +1,33 @@
+const IdeasSummary = async () => {
+  // to make
+  // const summary = await getIdeasSumary()
+  // if (!summary) return <GetIdeasSummaryButton />
+
+  return (
+    <div className="space-y-3 px-5">
+      <h3 className="text-center text-2xl font-semibold leading-none tracking-tight p-3">
+        Company Summary
+      </h3>
+      <p className="text-muted-foreground text-md leading-normal ">
+        Content of the analysis of user profile and and company goals, problems
+        and opportunuties and ideas for projects to offer to that company to
+        address those chances for improvement. Content of the analysis of user
+        profile and and company goals, problems and opportunuties and ideas for
+        projects to offer to that company to address those chances for
+        improvement. Content of the analysis of user profile and and company
+        goals, problems and opportunuties and ideas for projects to offer to
+        that company to address those chances for improvement.
+      </p>
+      <p className="text-muted-foreground text-md leading-normal">
+        Content of the analysis of user profile and and company goals, problems
+        and opportunuties and ideas for projects to offer to that company to
+        address those chances for improvement. Content of the analysis of user
+        profile and and company goals, problems and opportunuties and ideas for
+        projects to offer to that company to address those chances for
+        improvement.
+      </p>
+    </div>
+  );
+};
+
+export default IdeasSummary;
