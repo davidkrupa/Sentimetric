a79a8b29f4537a85d01dd8d191c4f37d9109943f
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-02-14 22:44:50 +0100

    getUser added from skills actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
+import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
 
-import { CreateUserParams, UpdateUserParams } from "@/types";
+import { CreateUserParams, UpdateUserParams, UserParams } from "@/types";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import { handleError } from "../utils";
@@ -13,7 +14,8 @@ export const createUser = async (user: CreateUserParams) => {
 
     const newUser = await User.create(user);
 
-    // ensure the newUser object is of correct format
+    if (!newUser) throw new Error("Error creating user");
+
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
@@ -26,9 +28,30 @@ export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
 
     const updatedUser = await User.findOneAndUpdate({ clerkId }, user);
 
-    if (!updatedUser) throw new Error("Error during updating user");
+    if (!updatedUser) throw new Error("Error updating user");
 
     return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const updateUserCurrentProfile = async (id: string): Promise<void> => {
+  try {
+    await connectToDatabase();
+
+    const { userId }: { userId: string | null } = auth();
+
+    if (!userId) throw new Error("User not authorized");
+
+    const updatedUser = await User.findOneAndUpdate(
+      { clerkId: userId },
+      { currentProfile: id }
+    );
+
+    if (!updatedUser) throw new Error("User not updated");
+
+    revalidatePath("/dashboard");
   } catch (error) {
     handleError(error);
   }
@@ -40,7 +63,7 @@ export const deleteUser = async (clerkId: string) => {
 
     const deletedUser = await User.findOneAndDelete({ clerkId });
 
-    if (!deletedUser) throw new Error("Error during deleting user");
+    if (!deletedUser) throw new Error("Error deleting user");
 
     revalidatePath("/");
 
@@ -48,4 +71,16 @@ export const deleteUser = async (clerkId: string) => {
   } catch (error) {
     handleError(error);
   }
+};
+
+export const getCurrentUser = async (): Promise<UserParams> => {
+  const { userId }: { userId: string | null } = auth();
+
+  if (!userId) throw new Error("User not authorized");
+
+  const user = await User.findOne({ clerkId: userId });
+
+  if (!user) throw new Error(`User not found with Clerk Id: ${userId}`);
+
+  return user;
 };
