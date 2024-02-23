abc24e53d77a63426620bcb1605687b1d3cb7b7a
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-02-23 23:11:31 +0100

    loading indicator when changin profile

--- components/SelectMenu.tsx
+++ components/SelectMenu.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ImSpinner2 } from "react-icons/im";
+
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -12,16 +14,30 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { updateUserCurrentProfile } from "@/lib/actions/user.actions";
 import { SelectMenuProps } from "@/types";
+import { useEffect, useState } from "react";
 const SelectMenu = ({ data, currentProfile }: SelectMenuProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [data, currentProfile]);
+
   const handleChange = async (e: string) => {
+    setIsLoading(true);
     await updateUserCurrentProfile(e);
   };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="capitalize block truncate w-48">
-          {currentProfile?.jobTitle}
+        <Button
+          variant="outline"
+          className="capitalize flex truncate w-48 gap relative px-6"
+        >
+          <p className="truncate">{currentProfile?.jobTitle}</p>
+          {isLoading && (
+            <ImSpinner2 className="animate-spin-slow h-5 w-5 mr-3 text-primary absolute right-0 top-0 bottom-0 my-auto" />
+          )}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
