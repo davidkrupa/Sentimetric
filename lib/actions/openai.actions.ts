1d55962804b98e9d719a8ad007514c1c984abf28
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-03-06 22:36:17 +0100

    projects array in ideas schema

--- lib/actions/openai.actions.ts
+++ lib/actions/openai.actions.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import OpenAI from "openai";
 import { handleError } from "../utils";
@@ -9,19 +9,20 @@ if (!apiKey) throw new Error("apiKey is missing");
 
 const openai = new OpenAI({ apiKey });
 
-export async function getAiResponse(text: string) {
+export async function getAiResponse(text: string): Promise<string> {
   try {
     const response = await openai.chat.completions.create({
       messages: [{ role: "user", content: text }],
       model: "gpt-3.5-turbo-1106",
     });
-    
+
     const answer = response.choices[0].message.content;
-  
+
     if (!answer) throw new Error("Error getting answer from openai");
-  
-    return answer;        
+
+    return answer;
   } catch (error) {
-    handleError(error)
+    console.error(error);
+    throw new Error("Error getting answer from openai");
   }
 }
--- lib/database/models/ideas.model.ts
+++ lib/database/models/ideas.model.ts
@@ -2,6 +2,7 @@ import { Schema, model, models } from "mongoose";
 
 const IdeasSchema = new Schema({
   content: { type: String, required: true },
+  projects: [{ type: String }],
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   profileId: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
   createdAt: { type: Date, default: Date.now },
