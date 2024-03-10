517a4ef8b72062d98d58c63b66c77c7e4461cab5
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-03-10 20:59:34 +0100

    get projects action and moved action from ideas file with update

--- lib/actions/project.actions.ts
+++ lib/actions/project.actions.ts
@@ -4,8 +4,11 @@ import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database";
 import Project from "../database/models/project.model";
 import { getCurrentUser } from "./user.actions";
+import { ProjectsData } from "@/types";
 
-const createDefaultProjectsTopics = async (content: string): Promise<void> => {
+export const createProjectsTopicsFromContent = async (
+  content: string
+): Promise<void> => {
   try {
     await connectToDatabase();
 
@@ -19,14 +22,56 @@ const createDefaultProjectsTopics = async (content: string): Promise<void> => {
       projects.push({
         name: match[1],
         topic: match[1],
-        content: "",
         userId: user._id,
         profileId: user.currentProfile,
       });
     }
 
-    const projectTitles = await Project.insertMany(projects);
+    if (projects.length === 0) return;
+
+    await Project.insertMany(projects);
 
     revalidatePath("/dashboard/ideas");
-  } catch (error) {}
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getProjects = async (): Promise<ProjectsData[] | undefined> => {
+  try {
+    await connectToDatabase();
+
+    const user = await getCurrentUser();
+
+    const projects = await Project.find({
+      userId: user._id,
+      profileId: user.currentProfile,
+    });
+
+    if (projects.length === 0) return;
+
+    return JSON.parse(JSON.stringify(projects));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteOneProject = async (id: string): Promise<void> => {
+  try {
+    await connectToDatabase();
+
+    const user = await getCurrentUser();
+
+    const project = await Project.findOneAndDelete({
+      userId: user._id,
+      profileId: user.currentProfile,
+      _id: id,
+    });
+
+    if (!project) throw new Error("Error deleting one project");
+
+    revalidatePath("/dashboard");
+  } catch (error) {
+    throw new Error("Error deleting one project");
+  }
 };
