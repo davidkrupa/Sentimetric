364d58483f3dc37adbdc1ab49b30890902651dee
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-03-16 23:45:50 +0100

    including undefined return value of a function in a component

--- components/IdeasSummary.tsx
+++ components/IdeasSummary.tsx
@@ -2,14 +2,14 @@ import { getIdeas } from "@/lib/actions/ideas.actions";
 import GetIdeasButton from "./GetIdeasButton";
 
 const IdeasSummary = async () => {
-  const { content } = await getIdeas();
+  const ideas = await getIdeas();
 
   return (
     <div className="space-y-3 px-5">
       <h3 className="text-center text-2xl font-semibold leading-none tracking-tight p-3">
         Company Summary
       </h3>
-      <p className="whitespace-pre-line">{content && content}</p>
+      <p className="whitespace-pre-line">{ideas?.content}</p>
       <GetIdeasButton />
     </div>
   );
