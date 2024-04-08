0bfffe9be77b4adb62a99fcc671363c134504156
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-08 22:48:05 +0200

    improved behaviour of sidebar on mobile and large devices

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,8 +13,6 @@ import { ImSpinner2 } from "react-icons/im";
 
 import logo from "../lib/assets/logo-first.png";
 
-const MIN_WIDTH = 768;
-
 const menuItems = [
   {
     title: "Research",
@@ -48,90 +46,89 @@ const menuItems = [
   },
 ];
 
+type SidebarOptions = "default" | "closed" | "opened";
+
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [chosenPath, setChosenPath] = useState("");
+  const [sidebarState, setSidebarState] = useState<SidebarOptions>("default");
+  const [delayTimeout, setDelayTimeout] = useState<NodeJS.Timeout | null>(null);
   const path = usePathname();
 
   useEffect(() => {
-    if (window.innerWidth >= MIN_WIDTH) {
-      setIsOpen(true);
-    } else {
-      setIsMobile(true);
+    if (delayTimeout !== null) {
+      clearTimeout(delayTimeout);
     }
-  }, []);
-
-  useEffect(() => {
     setIsLoading(false);
   }, [path]);
 
   const handleLinkClick = (clickedPath: string) => {
     if (chosenPath === clickedPath) return;
 
-    setIsLoading(true);
     setChosenPath(clickedPath);
-    if (isMobile) {
-      setIsOpen(false);
-    }
+    setSidebarState("default");
+
+    // delay for preventing spinner blink for cached routes
+    const timeout = setTimeout(() => {
+      setIsLoading(true);
+    }, 200);
+
+    setDelayTimeout(timeout);
+  };
+
+  const sidebarStyle = {
+    default: "hidden md:block",
+    closed: "hidden",
+    opened: "block",
   };
 
   return (
     <>
-      {/* opened sidebar */}
-      {isOpen && (
-        <aside className="md:static fixed top-0 left-0 z-20 bg-card w-52 min-h-screen pb-10 md:overflow-hidden overflow-auto md:hover:overflow-auto border-r border-r-border">
-          <div className="flex justify-between items-center p-4">
-            <div className="flex justify-start items-center gap-2">
-              <Image src={logo} alt="Sentimetric logo" width={25} height={25} />
-              <span>Sentimetric</span>
+      {/* sidebar */}
+      <aside
+        className={`${sidebarStyle[sidebarState]} md:static fixed top-0 left-0 z-20 bg-card w-52 min-h-screen pb-10 md:overflow-hidden overflow-auto md:hover:overflow-auto border-r border-r-border`}
+      >
+        <div className="flex justify-between items-center p-4">
+          <div className="flex justify-start items-center gap-2">
+            <Image src={logo} alt="Sentimetric logo" width={25} height={25} />
+            <span>Sentimetric</span>
+          </div>
+          <button onClick={() => setSidebarState("closed")}>
+            <RiMenuFoldLine />
+          </button>
+        </div>
+        <div className="flex flex-col px-4 pt-4">
+          {menuItems.map((item) => (
+            <div className="flex flex-col gap-3" key={item.title}>
+              {item.list.map((tool) => (
+                <Link
+                  key={tool.title}
+                  href={tool.path}
+                  onClick={() => handleLinkClick(tool.path)}
+                  className={`flex items-center justify-start relative gap-5 py-3 px-2 rounded-lg text-muted-foreground hover:text-foreground text-md w-full hover:bg-muted/50 transition-colors ${
+                    tool.path === path && "bg-muted/50"
+                  }`}
+                >
+                  {tool.icon}
+                  <span className="capitalize">{tool.title}</span>
+                  {isLoading && tool.path === chosenPath && (
+                    <ImSpinner2 className="animate-spin-slow h-5 w-5 mr-3 text-primary absolute right-0 top-0 bottom-0 my-auto" />
+                  )}
+                </Link>
+              ))}
             </div>
-            <button onClick={() => setIsOpen(false)}>
-              <RiMenuFoldLine />
-            </button>
-          </div>
-          <div className="flex flex-col px-4 pt-4">
-            {menuItems.map((item) => (
-              <div key={item.title}>
-                {/* grouping sidebar routes for the future */}
-                {/* <p className="text-muted-foreground text-sm uppercase">
-                  {item.title}
-                </p> */}
-                <div className="flex flex-col gap-3">
-                  {item.list.map((tool) => (
-                    <Link
-                      key={tool.title}
-                      href={tool.path}
-                      onClick={() => handleLinkClick(tool.path)}
-                      className={`flex items-center justify-start relative gap-5 py-3 px-2 rounded-lg text-muted-foreground hover:text-foreground text-md w-full hover:bg-muted/50 transition-colors ${
-                        tool.path === path && "bg-muted/50"
-                      }`}
-                    >
-                      {tool.icon}
-                      <span className="capitalize">{tool.title}</span>
-                      {isLoading && tool.path === chosenPath && (
-                        <ImSpinner2 className="animate-spin-slow h-5 w-5 mr-3 text-primary absolute right-0 top-0 bottom-0 my-auto" />
-                      )}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </aside>
-      )}
+          ))}
+        </div>
+      </aside>
 
-      {/* closed sidebar */}
-      {!isOpen && isMobile && (
-        <button
-          className="flex justify-center items-center fixed z-10 bottom-2 left-2 p-2 border-r border-t border-b border-input rounded-full bg-primary text-white"
-          onClick={() => setIsOpen(true)}
-        >
-          {!isLoading && <RiMenuUnfoldLine />}
-          {isLoading && <ImSpinner2 className="animate-spin-slow text-white" />}
-        </button>
-      )}
+      {/* sidebar toggle button */}
+      <button
+        className="flex justify-center items-center fixed z-10 bottom-2 left-2 p-2 border-r border-t border-b border-input rounded-full bg-primary text-white"
+        onClick={() => setSidebarState("opened")}
+      >
+        {!isLoading && <RiMenuUnfoldLine />}
+        {isLoading && <ImSpinner2 className="animate-spin-slow text-white" />}
+      </button>
     </>
   );
 };
