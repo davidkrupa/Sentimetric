78d948b2662f737e2a6fc140b46ed766cb6b4141
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-10 23:41:00 +0200

    small changes in UI

--- components/LineChartCard.tsx
+++ components/LineChartCard.tsx
@@ -1,14 +1,25 @@
 import { getActivitiesAmountByDay } from "@/lib/actions/activities.actions";
 import ReChartsLineChart from "./ReChartsLineChart";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 const LineChartCard = async () => {
   const data = await getActivitiesAmountByDay(7);
 
+  const amount = data?.reduce((acc, curr) => acc + curr?.activities, 0);
+
   return (
     <Card className="lg:col-span-4">
       <CardHeader>
-        <CardTitle>Summary</CardTitle>
+        <CardTitle>Last Week's Activities</CardTitle>
+        <CardDescription>
+          You had {amount || 0} activities in the last 7 days.
+        </CardDescription>
       </CardHeader>
       <CardContent className="pl-2">
         <ReChartsLineChart data={data} />
