b464da0a3962be356db7fbf018b400426e9876e8
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-17 11:59:41 +0200

    reusable spinner component

---
+++ components/ui/LoadingSpinner.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { ImSpinner2 } from "react-icons/im";
+
+type LoadingSpinnerProps = {
+  className?: string;
+};
+
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ className }) => {
+  return (
+    <ImSpinner2
+      className={cn("animate-spin-slow h-5 w-5 text-primary", className)}
+    />
+  );
+};
+
+export default LoadingSpinner;
