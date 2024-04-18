6fd4656542023a9da5fa9cea4d3fda6d48ff911f
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-18 15:22:04 +0200

    loading state for deleting skill

--- components/DeleteBadgeButton.tsx
+++ components/DeleteBadgeButton.tsx
@@ -3,12 +3,19 @@
 import { createActivity } from "@/lib/actions/activities.actions";
 import { deleteOneSkill } from "@/lib/actions/skills.actions";
 import { DeleteBadgeButtonProps } from "@/types";
-import { IoTrashOutline } from "react-icons/io5";
+import { useState } from "react";
+import { FaXmark } from "react-icons/fa6";
+
+import LoadingSpinner from "./ui/LoadingSpinner";
 
 const DeleteBadgeButton = ({ skill, type }: DeleteBadgeButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleDelete = async () => {
+    setIsLoading(true);
     await deleteOneSkill(skill, type);
     await createActivity("skill", "removed");
+    setIsLoading(false);
   };
 
   return (
@@ -16,7 +23,11 @@ const DeleteBadgeButton = ({ skill, type }: DeleteBadgeButtonProps) => {
       onClick={() => handleDelete()}
       className="absolute right-0 top-0 h-full w-full rounded-lg bg-muted/50 text-primary opacity-0 group-hover:opacity-100 transition-opacity"
     >
-      <IoTrashOutline className="text-sm text-primary mx-auto" />
+      {!isLoading ? (
+        <FaXmark className="text-sm text-destructive mx-auto" />
+      ) : (
+        <LoadingSpinner className="h-3 w-3 mx-auto" />
+      )}
     </button>
   );
 };
