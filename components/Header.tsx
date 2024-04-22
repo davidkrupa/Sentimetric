a7531ea78d0c7fb2625b21085e9d136d42ab1dd0
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-22 22:10:37 +0200

    header moved to hero section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
@@ -6,7 +6,7 @@ import logo from "../lib/assets/logo-first.png";
 
 const Header = () => {
   return (
-    <header className="w-full border-b p-2">
+    <header className="w-full p-2 absolute top-0 left-0">
       <div className="flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image src={logo} width={25} height={25} alt="Sentimetric logo" />
@@ -23,9 +23,12 @@ const Header = () => {
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
           <SignedOut>
-            <button className="rounded-full p-4 bg-red-500">
-              <Link href="/sign-in">Login</Link>
-            </button>
+            <Link
+              href="/sign-in"
+              className={buttonVariants({ variant: "default" })}
+            >
+              Sign In
+            </Link>
           </SignedOut>
         </div>
       </div>
