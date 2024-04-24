ba468aed968938c533a4b669615017e0690eddac
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-24 19:17:01 +0200

    added cards section to the homepage and reduced gap between sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import BackgroundGradient from "@/components/BackgroundGradient";
+import CardsSection from "@/components/CardsSection";
 import Hero from "@/components/Hero";
 import PricingCards from "@/components/PricingCards";
 import Timeline from "@/components/Timeline";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-32 w-full px-2 md:px-6 lg:px-12">
+    <main className="flex flex-col gap-16 w-full px-2 md:px-6 lg:px-12">
       <BackgroundGradient />
       <Hero />
       <Timeline />
+      <CardsSection />
       <PricingCards />
     </main>
   );
