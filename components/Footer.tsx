5c0877e19647a8e63f82f871b82b905d9e73dae3
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-25 10:40:04 +0200

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mx-auto text-muted py-6">©Sentimetric 2024</footer>
+  );
 };
 
 export default Footer;
