c22494afd6a491bafdbc1496152a24b48acff19e
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-25 10:39:38 +0200

    buttons in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,38 @@
 import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import logo from "../lib/assets/logo-first.png";
 
 const Header = () => {
   return (
-    <header className="w-full p-2 absolute top-0 left-0">
+    <header className="w-full py-4 absolute top-0 left-0">
       <div className="flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image src={logo} width={25} height={25} alt="Sentimetric logo" />
         </Link>
-
         <div className="flex justify-center items-center gap-3">
           <SignedIn>
-            <Link
-              href="/dashboard"
-              className={buttonVariants({ variant: "default" })}
-            >
-              Dashboard
-            </Link>
+            <Button asChild variant="default">
+              <Link href="/dashboard">Dashboard</Link>
+            </Button>
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
           <SignedOut>
-            <Link
-              href="/sign-in"
-              className={buttonVariants({ variant: "default" })}
+            <Button
+              asChild
+              variant="secondary"
+              className="hover:bg-primary hover:text-muted"
             >
-              Sign In
-            </Link>
+              <Link href="/sign-in">Sign In</Link>
+            </Button>
+            <Button
+              asChild
+              variant="outline"
+              className="bg-transparent border text-muted border-muted-foreground"
+            >
+              <Link href="/sign-up">Create Account</Link>
+            </Button>
           </SignedOut>
         </div>
       </div>
