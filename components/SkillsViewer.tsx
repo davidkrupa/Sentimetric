5dc0dbde4aa5ef0abb162f0f19feec46989c1df0
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-04-28 12:38:36 +0200

    skills viewer update

--- components/SkillsViewer.tsx
+++ components/SkillsViewer.tsx
@@ -2,47 +2,56 @@ import { getShortenedList } from "@/lib/utils";
 import ShowAllDialog from "./ShowAllDialog";
 import SkillsBadges from "./SkillsBadges";
 import { getSkills } from "@/lib/actions/skills.actions";
+import NoDataOrError from "./NoDataOrError";
 
 const SkillsViewer = async ({ chars }: { chars: number }) => {
-  const { hardSkills, softSkills } = await getSkills();
+  const {
+    data: { hardSkills, softSkills },
+    error,
+  } = await getSkills();
 
-  if (hardSkills?.length === 0 && softSkills?.length === 0)
-    return <p>No skills yet.</p>;
+  const isListEmpty = hardSkills.length === 0 && softSkills.length === 0;
 
   const shortenedHardSkills = getShortenedList(hardSkills, chars);
   const shortenedSoftSkills = getShortenedList(softSkills, chars);
 
   return (
-    <div className="space-y-3">
-      <div className="space-y-3">
-        <p className="text-sm font-medium">Hard Skills</p>
-        <SkillsBadges
-          skills={shortenedHardSkills}
-          type="hardSkills"
-          hasMore={hardSkills.length > shortenedHardSkills.length}
-        >
-          <ShowAllDialog
-            skills={hardSkills}
-            type="hardSkills"
-            hasMore={false}
-          />
-        </SkillsBadges>
-      </div>
-      <div className="space-y-3">
-        <p className="text-sm font-medium">Soft Skills</p>
-        <SkillsBadges
-          skills={shortenedSoftSkills}
-          type="softSkills"
-          hasMore={softSkills.length > shortenedSoftSkills.length}
-        >
-          <ShowAllDialog
-            skills={softSkills}
-            type="softSkills"
-            hasMore={false}
-          />
-        </SkillsBadges>
-      </div>
-    </div>
+    <>
+      {isListEmpty ? (
+        <NoDataOrError defaultText="No skills yet." error={error} />
+      ) : (
+        <div className="space-y-3">
+          <div className="space-y-3">
+            <p className="text-sm font-medium">Hard Skills</p>
+            <SkillsBadges
+              skills={shortenedHardSkills}
+              type="hardSkills"
+              hasMore={hardSkills.length > shortenedHardSkills.length}
+            >
+              <ShowAllDialog
+                skills={hardSkills}
+                type="hardSkills"
+                hasMore={false}
+              />
+            </SkillsBadges>
+          </div>
+          <div className="space-y-3">
+            <p className="text-sm font-medium">Soft Skills</p>
+            <SkillsBadges
+              skills={shortenedSoftSkills}
+              type="softSkills"
+              hasMore={softSkills.length > shortenedSoftSkills.length}
+            >
+              <ShowAllDialog
+                skills={softSkills}
+                type="softSkills"
+                hasMore={false}
+              />
+            </SkillsBadges>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
