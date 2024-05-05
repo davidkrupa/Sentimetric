0f3f344f61c227b0c76797556ea6a5b40c73c986
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-05-05 13:35:10 +0200

    update colors causing bug with dark theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mx-auto text-muted py-6">©Sentimetric 2024</footer>
+    <footer className="mx-auto text-slate-100 py-6">©Sentimetric 2024</footer>
   );
 };
 
