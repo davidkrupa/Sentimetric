b56943ad575113c1400fb0b37e6c08b6a53d66bc
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-05-07 18:30:38 +0200

    refactor ideas actions

--- lib/actions/ideas.actions.ts
+++ lib/actions/ideas.actions.ts
@@ -1,61 +1,76 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+
 import { connectToDatabase } from "../database";
 import CustomAnalysis from "../database/models/analysis.model";
 import Ideas from "../database/models/ideas.model";
 import Profile from "../database/models/profile.models";
+import JobSkills from "../database/models/skills.model";
 import { getAiResponse } from "./openai.actions";
 import { getCurrentUser } from "./user.actions";
-import JobSkills from "../database/models/skills.model";
 import { GetIdeas, VoidOrError } from "@/types";
 import { getErrorMessage } from "../utils";
 
-export const createIdeasFromProfile = async (): Promise<VoidOrError> => {
+export const getPromptVariables = async () => {
+  await connectToDatabase();
+
+  const user = await getCurrentUser();
+
+  const allAnalysis = await CustomAnalysis.find({
+    userId: user._id,
+    profileId: user.currentProfile,
+  });
+
+  if (allAnalysis.length === 0)
+    throw new Error("No analysis found for the current profile.");
+
+  // prepare all analysis content
+  let analysisPrompt = "";
+  for (let i = 0; i < allAnalysis.length; i++) {
+    analysisPrompt += `
+      Topic of the analysis: "${allAnalysis[i].topic}",
+      Content of the analysis: "${allAnalysis[i].content}"
+      `;
+  }
+
+  const profile = await Profile.findOne({ _id: user.currentProfile });
+
+  if (!profile) throw new Error("The current user profile was not found.");
+
+  const skills = await JobSkills.findOne({ profileId: user.currentProfile });
+
+  if (!skills) throw new Error("No skills found for the current user profile.");
+  if (skills.hardSkills.length === 0 && skills.softSkills.length === 0) {
+    throw new Error("You need to add skills first");
+  }
+
+  const skillsString = [...skills.hardSkills, ...skills.softSkills].join(", ");
+
+  return {
+    jobTitle: profile.jobTitle,
+    skills: skillsString,
+    company: profile.company,
+    industry: profile.industry,
+    content: analysisPrompt,
+  };
+};
+
+export const creteIdeasForCompany = async (): Promise<VoidOrError> => {
   try {
     await connectToDatabase();
 
     const user = await getCurrentUser();
 
-    const allAnalysis = await CustomAnalysis.find({
-      userId: user._id,
-      profileId: user.currentProfile,
-    });
-
-    if (allAnalysis.length === 0)
-      throw new Error("No analysis found for the current profile.");
-
-    // prepare analysis prompt
-    let analysisPrompt = "";
-    for (let i = 0; i < allAnalysis.length; i++) {
-      analysisPrompt += `
-      Topic of the analysis: "${allAnalysis[i].topic}",
-      Content of the analysis: "${allAnalysis[i].content}"
-      `;
-    }
-
-    const profile = await Profile.findOne({ _id: user.currentProfile });
-
-    if (!profile) throw new Error("The current user profile was not found.");
-
-    const skills = await JobSkills.findOne({ profileId: user.currentProfile });
-
-    if (!skills)
-      throw new Error("No skills found for the current user profile.");
-    if (skills.hardSkills.length === 0 && skills.softSkills.length === 0) {
-      throw new Error("You need to add skills first");
-    }
-
-    const skillsString = [...skills.hardSkills, ...skills.softSkills].join(
-      ", "
-    );
+    const { jobTitle, skills, company, industry, content } =
+      await getPromptVariables();
 
     const prompt = process.env
-      .COMPANY_ANALYSIS_PROMPT!.replace("{{jobTitle}}", profile.jobTitle)
-      .replace("{{skills}}", skillsString)
-      .replace("{{company}}", profile.company)
-      .replace("{{industry}}", profile.industry)
-      .replace("{{content}}", analysisPrompt);
+      .COMPANY_ANALYSIS_IDEAS_ONLY_PROMPT!.replace("{{jobTitle}}", jobTitle)
+      .replace("{{skills}}", skills)
+      .replace("{{company}}", company)
+      .replace("{{industry}}", industry)
+      .replace("{{content}}", content);
 
     const aiResponse = await getAiResponse(prompt);
 
