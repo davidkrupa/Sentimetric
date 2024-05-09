6ae0ddf863efb306908d63a38de0ae89b1cd7bd1
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-05-09 15:00:23 +0200

    function to format text

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 import { toast } from "@/components/ui/use-toast";
+import { FormatTextResults } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -52,4 +53,31 @@ export const showToastError = (errorMessage: string) => {
     title: "Something went wrong!",
     description: errorMessage,
   });
+};
+
+export const formatText = (text: string | undefined) => {
+  if (!text) return [];
+  // Split text into lines
+  const lines = text.split("\n");
+
+  let results: FormatTextResults[] = [];
+
+  // Loop through each line
+  lines.forEach((line) => {
+    // Check if the line starts with a number followed by a period and space
+    if (/^\d+\.\s/.test(line.trim())) {
+      // Split line by " - " to separate title and explanation
+      const [indexAndTitle, explanation] = line.split(" - ");
+      // Extract index and title
+      const [index, title] = indexAndTitle.split(". ");
+      // Push into results array
+      results.push({
+        index: parseInt(index),
+        title: title?.trim(),
+        explanation: explanation?.trim(),
+      });
+    }
+  });
+
+  return results;
 };
