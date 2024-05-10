b3f83df4697a432b82b80bdfc7d910e5311076dc
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-05-10 10:11:57 +0200

    scroll area for content section

--- components/GenerateContentSection.tsx
+++ components/GenerateContentSection.tsx
@@ -1,33 +1,32 @@
+import { getProjectSection } from "@/lib/actions/project.actions";
 import { cn } from "@/lib/utils";
-
-type GenerateContentSectionProps = {
-  className?: string;
-  children?: React.ReactNode;
-  title: string;
-  sectionId: string;
-};
+import { GenerateContentSectionProps } from "@/types";
+import { ScrollArea } from "./ui/scroll-area";
 
 const GenerateContentSection: React.FC<GenerateContentSectionProps> = async ({
   className,
   children,
   title,
-  sectionId,
+  sectionType,
+  sectionIndex,
 }) => {
-  // const response = await getSectionContent(sectionId);
+  const { data, error } = await getProjectSection(sectionType, sectionIndex);
 
   return (
     <div
       className={cn(
-        "flex flex-col md:flex-row justify-center max-w-5xl gap-5 p-4 border rounded-lg shadow-sm mx-auto ",
+        "flex flex-col lg:flex-row justify-center max-w-5xl gap-5 p-4 border rounded-lg shadow-sm mx-auto ",
         className
       )}
     >
-      <div className="flex flex-col justify-center items-center w-full md:w-1/2 lg:w-1/3 gap-3 rounded-lg bg-card text-card-foreground py-8 mx-auto border shadow-sm">
+      <div className="flex flex-col justify-center items-center w-full lg:w-1/3 gap-3 rounded-lg bg-card text-card-foreground py-8 mx-auto border shadow-sm">
         <h3 className="text-lg text-muted-foreground">{title}</h3>
         {children}
       </div>
-      <div className="w-full md:w-1/2 lg:w-2/3 bg-muted/50 rounded-lg text-card-foreground py-8  shadow-sm">
-        <p className="text-center">Text content from AI</p>
+      <div className="w-full lg:w-2/3 bg-muted/50 rounded-lg text-card-foreground py-6 px-2 shadow-sm">
+        <ScrollArea className="h-52 text-muted-foreground leading-normal">
+          <p className="text-justify px-6 sm:px-8">{data?.content}</p>
+        </ScrollArea>
       </div>
     </div>
   );
