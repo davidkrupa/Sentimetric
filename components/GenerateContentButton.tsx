91367d51a00257da7c78283439409098a103c19c
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-05-11 15:22:00 +0200

    loading state for GenerateContentButton

--- components/GenerateContentButton.tsx
+++ components/GenerateContentButton.tsx
@@ -1,31 +1,43 @@
 "use client";
 
+import { useState } from "react";
+
 import {
   createProjectIdeaSection,
   createProjectSection,
 } from "@/lib/actions/project.actions";
 import { Button } from "./ui/button";
+import LoadingSpinner from "./ui/LoadingSpinner";
 
 const GenerateContentButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const handleGenerateContent = async () => {
+    setIsLoading(true);
     await Promise.all([
-      createProjectSection("introduction"),
-      createProjectSection("about"),
-      createProjectSection("conclusion"),
+      createProjectSection("introduction", "Introduction"),
+      createProjectSection("about", "About Me"),
+      createProjectSection("conclusion", "Conclusion"),
       createProjectIdeaSection(0),
       createProjectIdeaSection(1),
       createProjectIdeaSection(2),
     ]);
+    setIsLoading(false);
   };
 
   return (
-    <Button
-      variant="default"
-      className="mx-auto"
-      onClick={() => handleGenerateContent()}
-    >
-      Generate Project
-    </Button>
+    <div className="relative">
+      <Button
+        variant="default"
+        className="mx-auto"
+        disabled={isLoading}
+        onClick={() => handleGenerateContent()}
+      >
+        Generate Project
+      </Button>
+      {isLoading && (
+        <LoadingSpinner className="absolute top-0 bottom-0 -right-8 my-auto" />
+      )}
+    </div>
   );
 };
 
