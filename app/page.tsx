5c8c3c27ed87fbc1e38365bb5793c31760b585a2
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-07-08 23:43:01 +0200

    removed video from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <PainAndBenefits />
       <Timeline />
-      <VideoTutorial />
+      {/* <VideoTutorial /> */}
       <CardsSection />
       <PricingCards />
       <Footer />
